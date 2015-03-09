436d28039aa0dfb7b8405bde662e7198e06a7f61
Erik de Jonge <erik@a8.nl>
2015-03-09 18:09:30 +0100

    py27dock Monday 09 March 2015 (week:10 day:68), 18:09:28

---
+++ main.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+"""
+py27doctest
+-
+Active8 (09-03-15)
+author: erik@a8.nl 
+license: GNU-GPL2
+"""
+import sys
+
+def main():
+    """
+    main
+    """
+
+    print()
+    print(sys.version)
+    print()
+
+
+if __name__ == "__main__":
+    main()
+    
